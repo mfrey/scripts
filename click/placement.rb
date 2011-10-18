9c76f2cd13656a50094c215bc2e036fd215b8632
Michael Frey <frey@informatik.hu-berlin.de>
2011-10-18 19:01:39 +0200

    Added first script for node placement

---
+++ click/placement.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby1.8
+
+require "gnuplot"
+
+class Coordinate
+  attr_accessor :x, :y, :z, :name
+
+  def initialize(x, y, z, name)
+    @x = x
+    @y = y
+    @z = z
+  end
+end
+
+class Placement
+  attr_accessor :coordinates
+
+  def initialize(filename)
+    begin 
+      file = File.new(filename, "r")
+      
+      @coordinates = Array.new
+
+      while(line = file.gets)
+        split = line.split(" ")
+        
+        if split.size == 4
+          @coordinates.push(Coordinate.new(split[1], split[2], split[3], split[4]))
+        end
+      end
+
+      file.close
+    rescue => error    
+      puts "Exception: #{error}"
+      error
+    end
+  end
+
+end
+
+placement = Placement.new("placementfile.plm")
+#placement.coordinates.each { |x| puts x.x.to_s + " " + x.y.to_s + " " + x.z.to_s } 
+
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new(gp) do |plot|
+  
+    plot.title  "Placement"
+    plot.ylabel "y"
+    plot.xlabel "x"
+    plot.set    "border linewidth 1.5"
+    plot.set    "pointsize 1.5"
+    plot.set    "style line 1 lc rgb '#0060ad' pt 7"
+
+    x = Array.new
+    y = Array.new
+    z = Array.new
+
+    placement.coordinates.each { |a| x.push(a.x.to_f) } 
+    placement.coordinates.each { |b| y.push(b.y.to_f) } 
+    placement.coordinates.each { |c| z.push(c.z.to_f) } 
+
+    plot.data << Gnuplot::DataSet.new( [x, y, z] ) do |ds|
+      ds.with = "p ls 1"
+    end
+  end
+end
+
+
