278b55395a42e6bc037dc0dc2895e216ea64fc81
Michael Frey <frey@informatik.hu-berlin.de>
2014-11-03 19:59:01 +0100

    added support for writing transformed log files for libARA (testbed) logfiles

--- transmogrifier.py
+++ transmogrifier.py
@@ -19,14 +19,15 @@ class LibAraLogFileParser:
         for line in input_file:
           match = regular_expression.finditer(line)
 
+          modified_line = line
+
           if match:
             for mac in match:
               hostname = self._get_node(line[mac.start() : mac.end()])
-              print(hostname)
-#              line = line.replace(line[mac.start() : mac.end()], hostname, 1)
-      #      print(line[mac.start() : mac.end()])
+              # let's replace the mac addresses with the hostnames
+              modified_line = modified_line.replace(line[mac.start() : mac.end()], hostname, 1)
 
-          output_file.write(line)
+          output_file.write(modified_line)
 
   def _get_node(self, mac):
     if mac not in self.nodes:
@@ -42,8 +43,13 @@ class LibAraLogFileParser:
           print("unknown mac address: " + mac)
           # we simply store the mac address as hostname
           hostname = mac
+        else:
+          hostname += '.[tap]'
+      else:
+        hostname += '.[wifi]'
       
-      self.nodes[mac] = hostname.strip('\n')
+      hostname = hostname.replace('\n', '')
+      self.nodes[mac] = hostname
      
     return self.nodes[mac]
 
@@ -55,8 +61,8 @@ class LibAraLogFileParser:
 
 def main():
   parser = argparse.ArgumentParser(description="transmogrifier - a script for transforming libARA (testbed) logs")
-  parser.add_argument('-i', '--input', dest='input', default=False, const=True, action='store_const', help='input file to be transformed')
-  parser.add_argument('-o', '--output', dest='output', default=False, const=True, action='store_const', help='output file for transformation')
+  parser.add_argument('-i', '--input', dest='input', type=str, default='', action='store', help='input file to be transformed')
+  parser.add_argument('-o', '--output', dest='output', type=str, default='', action='store', help='output file for transformation')
 
   if len(sys.argv) == 1:
     parser.print_help()
@@ -65,7 +71,7 @@ def main():
   arguments = parser.parse_args()
 
   log = LibAraLogFileParser()
-  log.transform()
+  log.transform(arguments.input, arguments.output)
 
 if __name__ == "__main__":
   main()
